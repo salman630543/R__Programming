cd8749e14891d923831295949979c763c77bb25e
salman630543 <112232446+salman630543@users.noreply.github.com>
2023-08-10 12:59:55 +0530

    Add files via upload

---
+++ (20)Empty plot with graph limits.r
@@ -0,0 +1,5 @@
+#print("Empty plot:")
+plot.new()
+#print("Empty plot specify the axes limits of the graphic:")
+plot(1, type="n", xlab="", ylab="", xlim=c(0, 15), ylim=c(0, 20))
+ 
---
+++ (24)Create a two dimentional array.r
@@ -0,0 +1,5 @@
+a <- array(seq(from = 50, length.out = 15, by = 2), c(5, 3))
+print("Content of the array:")
+print("5×3 array of sequence of even integers greater than 50:")
+print(a)
+ 
